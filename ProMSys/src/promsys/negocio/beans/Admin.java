bed35b1167c5da02ef12c991b9186e8e6153276b
Gabriel Ferreira Melo <gmelo.cc@gmail.com>
2017-06-17 13:26:10 -0300

    Admin Beans 1

--- ProMSys/src/promsys/negocio/beans/Admin.java
+++ ProMSys/src/promsys/negocio/beans/Admin.java
@@ -1,5 +1,43 @@
 package promsys.negocio.beans;
 
 public class Admin {
+	
+	private static long nextID;
+	
+	private String login;
+	private String senha;
+	private long id;
+	
+	public Admin(String l, String s){
+		this.login = l;
+		this.senha = s;
+		this.id = nextID;
+		nextID++;
+	}
+	
+	
+	
+	//getters e setters
 
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public long getID(){
+		return this.id;
+	}
+	
+		
 }
