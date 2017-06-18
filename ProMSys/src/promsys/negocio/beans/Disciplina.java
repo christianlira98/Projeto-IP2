a8a82524475c34c7dd656fd90f4f507477a050b4
CleytonJRodrigues <>
2017-06-18 20:34:00 -0300

    Classe Disciplina com implementação inicial.

--- ProMSys/src/promsys/negocio/beans/Disciplina.java
+++ ProMSys/src/promsys/negocio/beans/Disciplina.java
@@ -2,4 +2,42 @@ package promsys.negocio.beans;
 
 public class Disciplina {
 
+	private long id;
+	private static long nextId = 1;
+	private String nome;
+	
+	public Disciplina(String nome) {
+		this.nome = nome;
+		this.id = nextId;
+		nextId++;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public boolean equals(Disciplina outra) {
+		
+		if(outra != null && outra instanceof Disciplina) {
+			
+			if(this.nome == outra.nome && this.id == outra.id){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return String.format("Nome: %s", this.nome);
+	}
+	
 }
