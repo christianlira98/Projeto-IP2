2410942ddb41e7078500198102567830a3c685e1
CleytonJRodrigues <cleytonjose98@outlook.com>
2017-06-18 18:23:13 -0300

    Classe Horario inicialmente implementada.

--- ProMSys/src/promsys/negocio/beans/Horario.java
+++ ProMSys/src/promsys/negocio/beans/Horario.java
@@ -1,5 +1,31 @@
 package promsys.negocio.beans;
 
+import java.time.LocalDateTime;
+
 public class Horario {
 
+	private LocalDateTime hora;
+	
+	public Horario(int hora) {
+		
+		this.hora = LocalDateTime.now().withHour(hora);
+	}
+
+	public LocalDateTime getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = LocalDateTime.now().withHour(hora);
+	}
+	
+	public boolean equals(Horario outro) {
+		return this.hora.equals(outro);
+	}
+	
+	public String toString() {
+		String hora = String.format("%d:%d", this.hora.getHour(), this.hora.getMinute() );
+		return hora;
+	}
+	
 }
