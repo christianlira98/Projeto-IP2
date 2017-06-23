1e46587500c79628d33f33774fa108eb054ff20b
Gabriel Ferreira Melo <gmelo.cc@gmail.com>
2017-06-23 15:48:55 -0300

    Servidor atualizado com a herança.

--- ProMSys/src/promsys/negocio/beans/Servidor.java
+++ ProMSys/src/promsys/negocio/beans/Servidor.java
@@ -4,8 +4,6 @@ public class Servidor extends Usuario {
 	
 	private static long nextID;
 	
-	private String login;
-	private String senha;
 	private long id;
 	
 	public Servidor(String n, String l, String s){
@@ -15,22 +13,6 @@ public class Servidor extends Usuario {
 	}
 	
 	//getters e setters
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
 	
 	public long getID(){
 		return this.id;
