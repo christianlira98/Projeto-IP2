4217ad538cef5d6a9c7fe6bcb15f2690fe9e4c46
CleytonJRodrigues <cleytonjose98@outlook.com>
2017-06-23 11:48:15 -0300

    Atualização da Classe Horario. Agora com atributos "horarioInicio" e "horarioFim".

--- ProMSys/src/promsys/negocio/beans/Horario.java
+++ ProMSys/src/promsys/negocio/beans/Horario.java
@@ -1,29 +1,47 @@
 package promsys.negocio.beans;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Horario {
 	
-	private LocalDateTime hora;
+	private LocalTime horarioInicio;
+	private LocalTime horarioFim;
 	
-	public Horario(int hora) {
-		this.hora = LocalDateTime.now().withHour(hora);
+	public Horario(int hora_inicio, int hora_fim) {
+		this.horarioInicio = LocalTime.of(hora_inicio, 0);
+		this.horarioFim = LocalTime.of(hora_fim, 0);
 	}
 
-	public LocalDateTime getHora() {
-		return hora;
+	public LocalTime getHorarioInicio() {
+		return this.horarioInicio;
+	}
+	
+	public LocalTime getHorarioFim() {
+		return this.horarioFim;
 	}
 
-	public void setHora(int hora) {
-		this.hora = LocalDateTime.now().withHour(hora);
+	public void setHorarioInicio(int hora, int minutos) {
+		this.horarioInicio = LocalTime.of(hora, minutos);
+	}
+	
+	public void setHorarioFim(int hora, int minutos) {
+		this.horarioFim = LocalTime.of(hora, minutos);
 	}
 	
 	public boolean equals(Horario outro) {
-		return this.hora.equals(outro);
+		
+		boolean result = false;
+		
+		if (outro instanceof Horario && outro != null) {
+			if( this.horarioInicio == outro.horarioInicio && this.horarioFim == outro.horarioFim ) {
+				result = true;
+			}
+		}
+		return result;
 	}
 	
 	public String toString() {
-		String hora = String.format("%d:%d", this.hora.getHour(), this.hora.getMinute() );
-		return hora;
+		String horario = String.format("Início: %s\nFim: %s", this.horarioInicio.toString(), this.horarioFim.toString() );
+		return horario;
 	}
 }
